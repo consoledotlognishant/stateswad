b7ec33fab4e042e6e8616d5bf3475856c22055b7
consoledotlognishant <myvivobook11@gmail.com>
2026-03-02 20:32:13 +0530

    updated backend

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -45,10 +45,12 @@ export const loginUser = handleAsyncError(async (req, res, next) => {
 
 // /Logout
 export const logout = handleAsyncError(async (req, res, next) => {
-    res.cookie('token', null, {
+    res.cookie("token", null, {
         expires: new Date(Date.now()),
-        httpOnly: true
-    })
+        httpOnly: true,
+        secure: true,
+        sameSite: "None"
+    });
     res.status(200).json({
         success: true,
         message: "Successfully Logged out"
