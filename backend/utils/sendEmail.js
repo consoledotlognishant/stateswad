b89b2173c0e9e4c5985a81fddd24e47767cb2c75
consoledotlognishant <myvivobook11@gmail.com>
2026-03-06 11:48:46 +0530

    updated

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,17 +1,25 @@
-import nodeMailer from 'nodemailer'
-export const sendEmail=async(options)=>{
-    const transporter=nodeMailer.createTransport({
-        service:process.env.SMTP_SERVICE,
-        auth:{
-            user:process.env.SMTP_MAIL,
-            pass:process.env.SMTP_PASSWORD
+import nodemailer from "nodemailer";
+
+export const sendEmail = async (options) => {
+
+    const transporter = nodemailer.createTransport({
+        service: process.env.SMTP_SERVICE,
+        secure: true,
+        auth: {
+            user: process.env.SMTP_MAIL,
+            pass: process.env.SMTP_PASSWORD
         }
-    })
-    const mailOptions={
-        from:process.env.SMTP_MAIL,
-        to:options.email,
-        subject:options.subject,
-        text:options.message
-    }
-    await transporter.sendMail(mailOptions);
-}
+    });
+
+    const mailOptions = {
+        from: process.env.SMTP_MAIL,
+        to: options.email,
+        subject: options.subject,
+        text: options.message
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+
+    console.log("Email sent:", info.response);
+
+};
