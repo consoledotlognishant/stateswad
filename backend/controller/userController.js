3e1a06b27237e45f6b415cc9221c7a01ac52f748
consoledotlognishant <myvivobook11@gmail.com>
2026-03-11 19:41:31 +0530

    check whuy not working mongodb

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -14,6 +14,7 @@ REGISTER USER
 ================================
 */
 export const registerUser = handleAsyncError(async (req, res, next) => {
+    console.log("REGISTER BODY:", req.body);
 
     const { name, email, password, avatar } = req.body;
 
